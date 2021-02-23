50efc9b2ff45fa42ac6b917b2af49e9f3c6f7395
jie <1282814653@qq.com>
2021-02-23 20:25:55 +0800

    建立全局的 components 文件

---
+++ src/components/Layout/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="layout">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Layout',
+    components: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    computed: {
+
+    },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
