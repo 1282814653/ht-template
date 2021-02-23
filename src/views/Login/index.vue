0e6d07c1f4cecbd5338cd8058c8df43d8be40d0a
刘世杰 <1282814653@qq.com>
2021-02-23 11:21:03 +0800

    基本的登录页面

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,19 +1,20 @@
 <template>
     <div class="login rel">
-        <div class="login-main abs">
-            <h1 class="title">登录页面</h1>
+        <div class="login-main abs borr10 bor1">
+            <h1 class="t-a-c  f-t-z f-w-b t-m-3-tb ">X X X 后台登录系统</h1>
             <div class="login-form">
-                <el-form :model="userInfo" status-icon ref="ruleForm" label-width="100px">
-                    <el-form-item label="用户名" prop="pass">
+                <el-form :model="userInfo" status-icon :rules="rules" label-position="left" ref="ruleForm" label-width="75px">
+                    <el-form-item label="用户名" prop="username">
                         <el-input v-model="userInfo.username" autocomplete="off"></el-input>
                     </el-form-item>
-                    <el-form-item label="密码" prop="checkPass">
+                    <el-form-item label="密码" prop="password">
                         <el-input type="password" v-model="userInfo.password" autocomplete="off"></el-input>
                     </el-form-item>
-                    <el-form-item>
-                        <el-button type="primary">登录</el-button>
-                    </el-form-item>
+
                 </el-form>
+            </div>
+            <div class="t-a-c">
+                <el-button type="primary" @click="userLogin()">登录</el-button>
             </div>
         </div>
     </div>
@@ -27,11 +28,36 @@ export default {
     },
     data() {
         return {
-            userInfo: {}
+            userInfo: {
+                username: "",
+                password: ""
+            },
+            rules: {
+                username: [
+                    { required: true, message: '请输入用户名', trigger: 'blur' },
+                    { min: 3, max: 11, message: '长度在 3 到 11 个字符', trigger: 'blur' }
+                ],
+                password: [
+                    { required: true, message: '请输入用户密码', trigger: 'change' },
+                    { min: 6, max: 16, message: '长度在 6 到 16 个字符', trigger: 'blur' }
+                ],
+            }
         };
     },
     methods: {
+        userLogin() {
+            if (this.userInfo.username === "123456" && this.userInfo.password === "123456") {
+                this.$router.push("/index")
+                localStorage.setItem("userLoginInfo", JSON.stringify(this.userInfo.username))
+                this.userInfo = {}
+            } else {
+                this.$message({
+                    message: '填写错误，请重新填写信息。',
+                    type: 'error'
+                });
+            }
 
+        }
     },
     created() {
 
@@ -50,15 +76,11 @@ export default {
 .login {
     width: 100%;
     height: 100%;
+    background-color: greenyellow;
     .login-main {
         width: 500px;
-        height: 400px;
-        .title {
-            width: 100%;
-            text-align: center;
-            font-size: 32px;
-            margin: 30px;
-        }
+        height: 350px;
+        padding: 40px;
     }
 }
 </style>
